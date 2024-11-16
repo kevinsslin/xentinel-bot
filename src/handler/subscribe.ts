c0cc89c8f084b60d125e21bf68a273d14b648f32
patty111 <smefyb@gmail.com>
2024-11-17 05:53:41 +0700

    adding subscribe to email

--- src/handler/subscribe.ts
+++ src/handler/subscribe.ts
@@ -3,6 +3,10 @@ import { createClient } from "@libsql/client";
 // Handler function to process game-related commands
 export async function handler(context: HandlerContext) {
 
+  if (`${process.env.webhook}` === "true") {
+    return;
+  }
+
   const turso = createClient({
       url: `${process.env.TURSO_DATABASE_URL}`,
       authToken: `${process.env.TURSO_AUTH_TOKEN}`,
@@ -14,7 +18,9 @@ export async function handler(context: HandlerContext) {
     },
   } = context;
 
+  const email = params?.email;
   const subscriber = context.message.sender.address;
+
   console.log(`Subscriber ${subscriber} requested to subscribe.`);
 
   try {
@@ -29,10 +35,31 @@ export async function handler(context: HandlerContext) {
       return;
     }
 
-    await turso.execute({
-      sql: "INSERT INTO subscribers (address, subscribe_time) VALUES ($sub, CURRENT_TIMESTAMP)",
-      args: { sub: subscriber },
-    });
+    if (email) {
+      const existingEmail = await turso.execute({
+        sql: "SELECT * FROM subscribers WHERE email = $email",
+        args: { email: email },
+      });
+
+      if (existingEmail.rows.length > 0) {
+        await context.reply(`Email ${email} is already associated with another subscriber.`);
+        console.log(`Email ${email} is already associated with another subscriber.`);
+        return;
+      }
+    }
+
+    if (email) {
+      await turso.execute({
+        sql: "INSERT INTO subscribers (address, email, subscribe_time) VALUES ($sub, $email, CURRENT_TIMESTAMP)",
+        args: { sub: subscriber, email: email },
+      });
+    } else {
+      await turso.execute({
+        sql: "INSERT INTO subscribers (address, subscribe_time) VALUES ($sub, CURRENT_TIMESTAMP)",
+        args: { sub: subscriber },
+      });
+    }
+    
     await context.reply(`Subscriber ${subscriber} added successfully.`);
     console.log(`Subscriber ${subscriber} added successfully.`);
   } catch (error) {
